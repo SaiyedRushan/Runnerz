762eba2879614bfd6c414462e6d644f227ca89c7
SaiyedRushan <rushan52@gmail.com>
2024-08-21 17:24:39 -0400

    Create UserHttpClient

---
+++ src/main/java/saiyed/rushan/runnerz/user/UserHttpClient.java
@@ -0,0 +1,13 @@
+package saiyed.rushan.runnerz.user;
+
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
+
+public interface UserHttpClient {
+    @GetExchange("/users")
+    List<User> findAll();
+
+    @GetExchange("/users/{id}")
+    User findById(Integer id);
+}
