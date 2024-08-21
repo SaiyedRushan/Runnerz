5e859d8fbffcc97eb29726b015001ea815806b7d
SaiyedRushan <rushan52@gmail.com>
2024-08-21 17:28:48 -0400

    create usercontroller and userhttpclient bean

--- src/main/java/saiyed/rushan/runnerz/RunnerzApplication.java
+++ src/main/java/saiyed/rushan/runnerz/RunnerzApplication.java
@@ -2,6 +2,11 @@ package saiyed.rushan.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import saiyed.rushan.runnerz.user.UserHttpClient;
 
 @SpringBootApplication
 public class RunnerzApplication {
@@ -10,4 +15,10 @@ public class RunnerzApplication {
 		SpringApplication.run(RunnerzApplication.class, args);
 	}
 
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("http://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
 }
